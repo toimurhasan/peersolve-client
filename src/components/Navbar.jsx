ec3ba6c522f744ed921b6f25a8fa3c656924c679
toimurhasan <2imur.hasan@gmail.com>
2025-06-04 11:22:51 +0600

    updated site title, favicon, and header-logo + footer-logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
             {links}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">PeerSolve</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{links}</ul>
