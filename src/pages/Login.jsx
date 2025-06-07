855bbe8e41ca34d6fbe3e635095d14e193da6eca
toimurhasan <2imur.hasan@gmail.com>
2025-06-07 11:26:26 +0600

    added firebase login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { use } from "react";
 import { FaGoogle } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
 import { Link } from "react-router";
+import AuthContext from "../contexts/AuthContext";
 
 const Login = () => {
+  const { signInUser } = use(AuthContext);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    signInUser(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        console.log(errorCode);
+      });
+  };
   return (
     <div className="flex justify-center items-center py-16">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
@@ -11,15 +28,17 @@ const Login = () => {
           <h1 className="text-4xl text-center text-transparent bg-clip-text pb-1 bg-gradient-to-r from-blue-600 via-orange-400 to-pink-400 font-bold">
             Login Form
           </h1>
-          <form className="fieldset">
+          <form onSubmit={handleSubmit} className="fieldset">
             <label className="label">Email</label>
             <input
+              name="email"
               type="email"
               className="input w-full focus:outline-transparent focus:border-gray-400 focus:shadow-xl"
               placeholder="Email"
             />
             <label className="label">Password</label>
             <input
+              name="password"
               type="password"
               className="input w-full focus:outline-transparent focus:border-gray-400 focus:shadow-xl"
               placeholder="Password"
