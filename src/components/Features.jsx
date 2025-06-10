7e6b8bac39af11278569519ec292e83fc9f8700f
toimurhasan <2imur.hasan@gmail.com>
2025-06-10 18:29:33 +0600

    features card hover scale 101

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BookOpenCheck, Users, PenLine, Star } from "lucide-react";
-import { motion } from "motion/react";
 
 const Features = () => {
   return (
@@ -11,68 +10,32 @@ const Features = () => {
 
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Feature 1 */}
-          <motion.div
-            whileHover={{
-              rotate: -5,
-              scale: 1.01,
-            }}
-            transition={{
-              duration: 0.01,
-            }}
-            className="bg-base-200  p-6 cursor-pointer select-none rounded-2xl shadow-md hover:shadow-xl transition"
-          >
+          <div className="bg-base-200 hover:scale-101  p-6 cursor-pointer select-none rounded-2xl shadow-md hover:shadow-xl transition">
             <Users className="h-10 w-10 mx-auto text-indigo-500 mb-4" />
             <h3 className="text-xl font-semibold mb-2">Study with Friends</h3>
             <p>Every user is automatically a friend—build your learning squad easily.</p>
-          </motion.div>
+          </div>
 
           {/* Feature 2 */}
-          <motion.div
-            whileHover={{
-              rotate: -5,
-              scale: 1.01,
-            }}
-            transition={{
-              duration: 0.01,
-            }}
-            className=" bg-base-200 p-6 cursor-pointer select-none rounded-2xl shadow-md hover:shadow-xl transition"
-          >
+          <div className=" bg-base-200 hover:scale-101 p-6 cursor-pointer select-none rounded-2xl shadow-md hover:shadow-xl transition">
             <PenLine className="h-10 w-10 mx-auto text-green-500 mb-4" />
             <h3 className="text-xl font-semibold mb-2">Create Assignments</h3>
             <p>Quickly set up group assignments and share them with everyone.</p>
-          </motion.div>
+          </div>
 
           {/* Feature 3 */}
-          <motion.div
-            whileHover={{
-              rotate: -5,
-              scale: 1.01,
-            }}
-            transition={{
-              duration: 0.01,
-            }}
-            className="bg-base-200 p-6 cursor-pointer select-none rounded-2xl shadow-md hover:shadow-xl transition"
-          >
+          <div className="bg-base-200 hover:scale-101 p-6 cursor-pointer select-none rounded-2xl shadow-md hover:shadow-xl transition">
             <BookOpenCheck className="h-10 w-10 mx-auto text-blue-500 mb-4" />
             <h3 className="text-xl font-semibold mb-2">Submit & Track</h3>
             <p>Submit your work and keep track of your progress in real-time.</p>
-          </motion.div>
+          </div>
 
           {/* Feature 4 */}
-          <motion.div
-            whileHover={{
-              rotate: -5,
-              scale: 1.01,
-            }}
-            transition={{
-              duration: 0.01,
-            }}
-            className=" xl:tooltip-right bg-base-200 p-6 cursor-pointer select-none rounded-2xl shadow-md hover:shadow-xl transition"
-          >
+          <div className=" hover:scale-101 bg-base-200 p-6 cursor-pointer select-none rounded-2xl shadow-md hover:shadow-xl transition">
             <Star className="h-10 w-10 mx-auto text-yellow-400 mb-4" />
             <h3 className="text-xl font-semibold mb-2">Peer Grading</h3>
             <p>Grade your friends' submissions and help each other improve.</p>
-          </motion.div>
+          </div>
         </div>
       </div>
     </section>
